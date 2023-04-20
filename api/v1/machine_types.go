9e4f36fdd5fd4379fa4a7f9c3aa7ad28987608b7
k.kumar <k.kumar@t-systems.com>
2023-04-20 20:28:09 +0530

    Add watches to controller to watch another k8s resource

---
+++ api/v1/machine_types.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MachineSpec defines the desired state of Machine
+type MachineSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Machine. Edit machine_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// NetworkInterfaceState is the infrastructure attachment state a NetworkInterface can be in.
+type NetworkInterfaceState string
+
+const (
+	// NetworkInterfaceStatePending indicates that the attachment of a network interface is pending.
+	NetworkInterfaceStatePending NetworkInterfaceState = "Pending"
+	// NetworkInterfaceStateAttached indicates that a network interface has been successfully attached.
+	NetworkInterfaceStateAttached NetworkInterfaceState = "Attached"
+	// NetworkInterfaceStateDetached indicates that a network interface has been successfully detached.
+	NetworkInterfaceStateDetached NetworkInterfaceState = "Detached"
+)
+
+type MachineState string
+
+const (
+	// MachineStatePending means the Machine has been accepted by the system, but not yet completely started.
+	// This includes time before being bound to a MachinePool, as well as time spent setting up the Machine on that
+	// MachinePool.
+	MachineStatePending MachineState = "Pending"
+	// MachineStateRunning means the machine is running on a MachinePool.
+	MachineStateRunning MachineState = "Running"
+	// MachineStateShutdown means the machine is shut down.
+	MachineStateShutdown MachineState = "Shutdown"
+	// MachineStateTerminated means the machine has been permanently stopped and cannot be started.
+	MachineStateTerminated MachineState = "Terminated"
+)
+
+// MachineStatus defines the observed state of Machine
+type MachineStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	State         MachineState          `json:"state,omitempty"`
+	NetworkStatus NetworkInterfaceState `json:"networkstate,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Machine is the Schema for the machines API
+type Machine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MachineSpec   `json:"spec,omitempty"`
+	Status MachineStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// MachineList contains a list of Machine
+type MachineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Machine `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Machine{}, &MachineList{})
+}
--- controllers/mycustomresource_controller.go
+++ controllers/mycustomresource_controller.go
@@ -24,9 +24,11 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
@@ -58,14 +60,18 @@ func (r *MyCustomResourceReconciler) Reconcile(ctx context.Context, req ctrl.Req
 
 	log.V(1).Info("Reconcile")
 
+	log.V(1).Info("Reconcile Resource", "Request", req)
+
 	myCustomResource := demov1.MyCustomResource{}
 	if err := r.Get(ctx, req.NamespacedName, &myCustomResource); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+
 	name, namespace := myCustomResource.Name, myCustomResource.Namespace
 	labels := map[string]string{
 		"foo": "bar",
 	}
+	log.V(1).Info("Get name and namesapce", "name:", name, "namespace:", namespace)
 
 	// pod to be created
 	desiredPod := corev1.Pod{
@@ -84,10 +90,10 @@ func (r *MyCustomResourceReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		},
 	}
 
-	log.V(1).Info("Set Controller Reference")
-	if err := controllerutil.SetControllerReference(&myCustomResource, &desiredPod, r.Scheme); err != nil {
-		return ctrl.Result{}, err
-	}
+	// log.V(1).Info("Set Controller Reference")
+	// if err := controllerutil.SetControllerReference(&myCustomResource, &desiredPod, r.Scheme); err != nil {
+	// 	return ctrl.Result{}, err
+	// }
 
 	foundPod := &corev1.Pod{}
 	if err := r.Get(ctx, client.ObjectKeyFromObject(&desiredPod), foundPod); err != nil {
@@ -129,6 +135,17 @@ func (r *MyCustomResourceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Watches(
 			&source.Kind{Type: &corev1.Pod{}},
 			&handler.EnqueueRequestForOwner{IsController: true, OwnerType: &demov1.MyCustomResource{}},
+		).
+		Watches(
+			&source.Kind{Type: &demov1.Machine{}},
+			handler.Funcs{
+				UpdateFunc: func(event event.UpdateEvent, queue workqueue.RateLimitingInterface) {
+					machine := event.ObjectNew.(*demov1.Machine)
+					if machine.Status.State == demov1.MachineStateRunning {
+						queue.Add(ctrl.Request{NamespacedName: types.NamespacedName{Name: machine.Name, Namespace: corev1.NamespaceDefault}})
+					}
+				},
+			},
 		).
 		Complete(r)
 }
